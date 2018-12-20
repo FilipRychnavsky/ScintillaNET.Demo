ac6e553c5ce77fe019fe35cf55a8e552a33f2362
Filip Rychnavsky <FRychnavsky@cvs.de>
2018-12-20 16:04:27 +0100

    Scintilla im Demo Projekt sichtbar

--- ScintillaNET.DemoFR/Form1.Designer.cs
+++ ScintillaNET.DemoFR/Form1.Designer.cs
@@ -28,12 +28,39 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.m_rPanel = new System.Windows.Forms.Panel();
+			this.SuspendLayout();
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+			// 
+			// m_rPanel
+			// 
+			this.m_rPanel.BackColor = System.Drawing.Color.LightSkyBlue;
+			this.m_rPanel.Location = new System.Drawing.Point(24, 125);
+			this.m_rPanel.Name = "m_rPanel";
+			this.m_rPanel.Size = new System.Drawing.Size(251, 261);
+			this.m_rPanel.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.m_rPanel);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.Panel m_rPanel;
 	}
 }
 
--- ScintillaNET.DemoFR/Form1.cs
+++ ScintillaNET.DemoFR/Form1.cs
@@ -7,14 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ScintillaNET;
 
 namespace ScintillaNET.DemoFR
 {
 	public partial class Form1 : Form
 	{
+		ScintillaNET.Scintilla m_rScintilla_TextArea;
+
 		public Form1()
 		{
 			InitializeComponent();
+			m_rScintilla_TextArea = new ScintillaNET.Scintilla();
+			m_rPanel.Controls.Add(m_rScintilla_TextArea);
 		}
 	}
 }
