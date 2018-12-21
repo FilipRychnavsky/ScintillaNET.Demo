546d9392753d6a67d2724b06319bfbd9d553c981
Filip Rychnavsky <FRychnavsky@cvs.de>
2018-12-21 10:43:04 +0100

    Lorem Ipsum und abc als demo Text

--- ScintillaNET.DemoFR/Form1.cs
+++ ScintillaNET.DemoFR/Form1.cs
@@ -22,7 +22,6 @@ namespace ScintillaNET.DemoFR
 			m_rPanel.Controls.Add(m_rScintilla_TextArea);
 			InitText();
 			InitDwelling();
-			SetIndicatorForURL();
 			m_rScintilla_TextArea.CharAdded += OnCharAdded;
 		}
 
@@ -43,43 +42,6 @@ namespace ScintillaNET.DemoFR
 			int nLengthEntered = nCurrentPosition - nWordStartPosition;
 			string sAutoCompletionList = "aaa bbb ccc dddd eee";			
 			m_rScintilla_TextArea.AutoCShow(nLengthEntered, sAutoCompletionList);
-		}
-
-		private void SetIndicatorForURL()
-		{
-			//TODO_FR 199 Implement ToolTip between DwellStart und DwellEnd events
-			//TODO_FR 299 ToolTip in AutoCompletion m_rScintilla_CodeEditor.AutoCShow(nLengthEntered, sAutoCompletionList);
-			//https://github.com/jacobslusser/ScintillaNET/issues/111
-			// Define an indicator for marking URLs and apply it to a range.
-			// How you determine a particular range is a URL and how often
-			// you want to scan the text for them is up to you.
-			m_rScintilla_TextArea.Indicators[0].Style = IndicatorStyle.Plain;
-			m_rScintilla_TextArea.Indicators[0].ForeColor = Color.Blue;
-			m_rScintilla_TextArea.IndicatorCurrent = 0;
-			m_rScintilla_TextArea.IndicatorFillRange(0, 21); // Use your own logic
-
-			// An indicator can only change the appearance of text in one way. So
-			// to get underlining AND a different foreground color we have to use two indicators.
-			m_rScintilla_TextArea.Indicators[1].Style = IndicatorStyle.TextFore;
-			m_rScintilla_TextArea.Indicators[1].ForeColor = Color.Blue;
-			m_rScintilla_TextArea.IndicatorCurrent = 1;
-			m_rScintilla_TextArea.IndicatorFillRange(0, 21); // Use your own logic
-
-
-/*
-			// Indicator 10 - out of Lexer indicators - Filip
-			m_rScintilla_TextArea.Indicators[10].Style = IndicatorStyle.TextFore;
-			m_rScintilla_TextArea.Indicators[10].ForeColor = Color.LightBlue;
-			m_rScintilla_TextArea.IndicatorCurrent = 10;
-			string sDemoTextRange = m_rScintilla_TextArea.GetTextRange(25, 30);
-			int nText_Length = m_rScintilla_TextArea.Text.Length;
-			int nTextLength = m_rScintilla_TextArea.TextLength; // before updating Cache - old size - https://github.com/jacobslusser/ScintillaNET/issues/223
-			m_rScintilla_TextArea.IndicatorFillRange(25,30);
-*/
-			m_rScintilla_TextArea.Styles[Style.CallTip].SizeF = 8.25F;
-			m_rScintilla_TextArea.Styles[Style.CallTip].ForeColor = SystemColors.InfoText;
-			m_rScintilla_TextArea.Styles[Style.CallTip].BackColor = SystemColors.Info;
-
 		}
 
 		private void InitDwelling()
@@ -109,12 +71,7 @@ namespace ScintillaNET.DemoFR
 
 		private void InitText()
 		{
-			// InitText
-			m_rScintilla_TextArea.Text = "http://www.google.com";
-			m_rScintilla_TextArea.CurrentPosition = 21;
-			m_rScintilla_TextArea.AppendText("\nhttp://www.izurnal.cz");
-			string sDebug_FistLine = m_rScintilla_TextArea.Lines[1].Text;
-			//m_rScintilla_TextArea.AddText("\r\nhttp://www.izurnal.cz");
+			m_rScintilla_TextArea.Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \nabc";
 		}
 
 	}
