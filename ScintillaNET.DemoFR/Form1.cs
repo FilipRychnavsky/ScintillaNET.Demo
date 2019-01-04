b4dc5bb365aa98e3b197e363618030c77d56e5b1
Filip Rychnavsky <FRychnavsky@cvs.de>
2019-01-04 13:48:56 +0100

    //Demoerweiterung des Tooltips

--- ScintillaNET.DemoFR/Form1.cs
+++ ScintillaNET.DemoFR/Form1.cs
@@ -139,7 +139,8 @@ namespace ScintillaNET.DemoFR
 			if (url != null) {
 				var callTip = string.Format("{0}\nCTRL + click to follow link", url);
 //TODO_FR 199 Teste Linebreaks in dem ToolTip
-
+//Demoerweiterung des Tooltips
+				callTip += "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 				m_rScintilla_TextArea.CallTipShow(e.Position, callTip);
 			}
 		}
